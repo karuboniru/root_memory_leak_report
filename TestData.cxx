f454e9875ba71c9528b5e8a5f289681d35e41877
Philippe Canal <pcanal@fnal.gov>
2024-09-06 11:26:30 -0500

    Move old I/O streamer to Custom Streamer (no change in behavior)

--- TestData.cxx
+++ TestData.cxx
@@ -1,6 +1,8 @@
 #include "TestData.h"
 #include "a_member.h"
 
+#include <TBuffer.h>
+
 ClassImp(TestData);
 
 TestData::~TestData() { delete sm; }
@@ -12,3 +14,22 @@ TestData::TestData() { Init(); }
 TestData::TestData(const TestData &other) = default;
 
 double TestData::Mag() const { return sm->get().Mag(); }
+
+//______________________________________________________________________________
+void TestData::Streamer(TBuffer &R__b)
+{
+   // Stream an object of class TestData.
+
+   UInt_t R__s, R__c;
+   if (R__b.IsReading()) {
+      Version_t R__v = R__b.ReadVersion(&R__s, &R__c); if (R__v) { }
+      TObject::Streamer(R__b);
+      R__b >> sm;
+      R__b.CheckByteCount(R__s, R__c, TestData::IsA());
+   } else {
+      R__c = R__b.WriteVersion(TestData::IsA(), kTRUE);
+      TObject::Streamer(R__b);
+      R__b << sm;
+      R__b.SetByteCount(R__c, kTRUE);
+   }
+}
